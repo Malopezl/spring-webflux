b856ad03f0809a9f14a5f810893bed76068dc103
Marvin Lopez <lopez.marvin9@gmail.com>
2024-09-10 21:24:10 -0600

    learn stream flux and subscribe method

--- spring-boot-reactor/src/main/java/gt/com/archteam/springboot/reactor/app/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/gt/com/archteam/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,31 @@
 package gt.com.archteam.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var nombres = Flux.just("Andres", "Pedro", "", "Juan").doOnNext(e -> {
+			if (e.isEmpty()) {
+				throw new RuntimeException("Nombres no pueden ser vacios");
+			}
+			System.out.println(e);
+		});
+		/* Si no se subscribe al objeto no va a mostrar nada... */
+		nombres.subscribe(log::info, error -> log.error(error.getMessage()));
 	}
 
 }
