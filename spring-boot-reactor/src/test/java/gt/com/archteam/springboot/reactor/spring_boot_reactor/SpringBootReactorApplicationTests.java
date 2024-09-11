a40d596bbc04e3c28b166c4c37e298044d8b6e66
Marvin Lopez <lopez.marvin9@gmail.com>
2024-09-10 18:12:35 -0600

    add base project

---
+++ spring-boot-reactor/src/test/java/gt/com/archteam/springboot/reactor/spring_boot_reactor/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.com.archteam.springboot.reactor.spring_boot_reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
